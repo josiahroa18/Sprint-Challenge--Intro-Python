764d73c039fcbc2f374c81f8cac98f3a1f51bc72
Josiah Roa <josiahroa18@gmail.com>
2020-07-10 10:48:36 -0600

    Finished stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -34,7 +34,6 @@ def cityreader(cities=[]):
     line_count = 0
     for row in city:
       if line_count > 0:
-        print(row)
         cities.append(City(row[0], float(row[3]), float(row[4])))
       line_count += 1
 
@@ -44,9 +43,9 @@ def cityreader(cities=[]):
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
-print(len(cities))
-for c in cities:
-    print(c)
+# print(len(cities))
+# for c in cities:
+#     print(c)
 
 # STRETCH GOAL!
 #
@@ -78,13 +77,72 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+# user_input_1 = input("Enter lat1,lon1: ")
+# user_input_2 = input("Enter lat2,lon2: ")
+# user_input_1 = user_input_1.split(',')
+# user_input_2 = user_input_2.split(',')
+# print(user_input_1)
+# print(user_input_2)
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
+  # Normalize input data to be a pair of lower-left,upper-right pair of coordinates
+  lat1 = float(lat1)
+  lon1 = float(lon1)
+  lat2 = float(lat2)
+  lon2 = float(lon2)
+
+  lower_left = []
+  upper_right = []
+  upper_left = []
+  lower_right = []
+  if lat1 < lat2 and lon1 < lon2:
+    lower_left = [lat1, lon1]
+    upper_right = [lat2, lon2]
+  elif lat1 > lat2 and lon1 > lon2:
+    lower_left = [lat2, lon2]
+    upper_right = [lat1, lon1]
+  elif lat1 < lat2 and lon1 > lon2:
+    upper_left = [lat1, lon1]
+    lower_right = [lat2, lon2]
+  elif lat1 > lat2 and lon1 < lon2:
+    upper_left = [lat1, lon1]
+    lower_right = [lat2, lon2]
+  else:
+    return "Error normalizing data"
+
   # within will hold the cities that fall within the specified region
   within = []
 
   # TODO Ensure that the lat and lon valuse are all floats
   # Go through each city and check to see if it falls within 
   # the specified coordinates.
+  for city in cities:
+    # Lower left and upper right
+    if len(lower_left) > 0 and len(upper_right) > 0:
+      lat1 = lower_left[0]
+      lon1 = lower_left[1]
+      lat2 = upper_right[0]
+      lon2 = upper_right[1]
+
+      if city.lat >= lat1 and city.lon >= lon1 and city.lat <= lat2 and city.lon <= lon2:
+        within.append(city)
+    # Upper left and lower right
+    else:
+      lat1 = upper_left[0]
+      lon1 = upper_left[1]
+      lat2 = lower_right[0]
+      lon2 = lower_right[1]
+
+      if city.lat <= lat1 and city.lon <= lon1 and city.lat >= lat2 and city.long >= lon2:
+        within.append(city)
+
+      
+  
 
   return within
+
+within_cities = cityreader_stretch(45, -100, 32.0, -120, cities)
+for city in within_cities:
+  print(city.name)
+# cityreader_stretch(user_input_1[0], user_input_1[1], user_input_2[0], user_input_2[1], cities)
+
